3847b196b7a39a8fd55002a774e1318e8e4b6f92
AdithyaPrasad-pro <adithya.insaaf@gmail.com>
2026-06-09 20:09:35 +0530

    Retain optimal feature set after feature engineering experiments

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ from sklearn.metrics import accuracy_score
 df = pd.read_csv("customer_churn.csv")
 
 x=df.drop(["RowNumber","Exited","CustomerId","Surname","Exited"],axis = 1)
-#x["BalancePerProduct"] = (x["Balance"]/x["NumOfProducts"].replace(0,1))
-
-#x["SalaryBalanceRatio"] = (x["EstimatedSalary"]/(x["Balance"]+1))
-
-#x["SeniorCitizen"] = (x["Age"] >= 60).astype(int)
-
-#x["LoyalCustomer"] = (x["Tenure"] >= 5).astype(int)
-
-#x["CreditIncomeRatio"] = (x["CreditScore"] /(x["EstimatedSalary"] + 1))
-
 
 y=df["Exited"]
 
